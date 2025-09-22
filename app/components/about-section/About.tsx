d2f05a8bf17e5d1d3788979a743af9ce9600eeda
Daniel Neves Fonseca <daniel.neves.fonseca.ti@gmail.com>
2025-09-22 19:06:43 -0300

    refactor: Simplify About section by removing unused imports and components

--- app/components/about-section/About.tsx
+++ app/components/about-section/About.tsx
@@ -1,38 +1,14 @@
-import React, { useEffect } from "react";
-import Link from "next/link";
-import { Syne } from "next/font/google";
-import { useView } from "@/contexts/ViewContext";
-import { useInView } from "react-intersection-observer";
-import AnimatedBody from "../ui/AnimatedBody";
-import AnimatedTitle from "../ui/AnimatedTitle";
-import ContraButton from "./ContraButton";
-
-const syne = Syne({ subsets: ["latin"] });
+import React from "react";
 
 export default function About() {
-  const { setSectionInView } = useView();
-
-  const { ref, inView } = useInView({
-    threshold: 0.2,
-    rootMargin: "-100px 0px",
-  });
-
-  useEffect(() => {
-    if (inView) setSectionInView("about");
-  }, [inView, setSectionInView]);
-
   return (
-    <section ref={ref} className="pt-24 md:pt-[150px]" id="about">
-      <AnimatedTitle
-        wordSpace={"mr-[14px]"}
-        charSpace={"mr-[0.001em]"}
-        className={`uppercase ${syne.className} antialiased text-4xl md:text-5xl xl:text-6xl font-bold opacity-80`}
-      >
+    <section className="pt-24 md:pt-[150px]" id="about">
+      <div className="uppercase antialiased text-4xl md:text-5xl xl:text-6xl font-bold opacity-80">
         I help companies to thrive with software.
-      </AnimatedTitle>
+      </div>
       <div className="grid grid-cols-1 lg:grid-cols-[8.5fr_3.5fr] gap-8 mt-6">
         <div className="grid grid-cols-1 antialiased gap-6 text-white/80 text-xl md:text-2xl">
-          <AnimatedBody className="leading-[34px] md:leading-[39px]">
+          <div className="leading-[34px] md:leading-[39px]">
             My lifelong passion for technology began with hardware and networks,
             understanding how systems work from the ground up. To build upon
             that foundation, I pursued a degree in Systems Analysis and
@@ -40,90 +16,69 @@ export default function About() {
             manage complex IT systems effectively. This blend of formal
             education and practical experience is the core of my problem-solving
             approach.
-          </AnimatedBody>
-          <AnimatedBody className="leading-[34px] md:leading-[39px]">
+          </div>
+          <div className="leading-[34px] md:leading-[39px]">
             Today, as a software engineer, I apply this complete perspective to
-            everything I build. Whether I'm freelancing on a new MVP for a
+            everything I build. Whether I&apos;m freelancing on a new MVP for a
             client or debugging a challenging issue, I am dedicated to creating
             solutions that are well-engineered from concept to deployment. I
             love making technology work seamlessly, and I have the full-stack
             skills to make it happen.
-          </AnimatedBody>
-          <AnimatedBody className="inline leading-[34px] md:leading-[39px]">
+          </div>
+          <div className="inline leading-[34px] md:leading-[39px]">
             Each challenge is unique so I ensure that I learn and grow through
             each one ensuring that I not only put in my best but also deliver
             solutions that businesses are proud to call their own. Wanna learn
             more? Here&apos;s <br className="hidden md:block" />
-            <Link className="underline" href="/resume_en.pdf">
+            <a className="underline" href="/resume_en.pdf">
               my résumè
-            </Link>
+            </a>
             .
-          </AnimatedBody>
+          </div>
         </div>
         <div className="grid grid-cols-1 gap-4">
           <div>
-            <AnimatedTitle
-              wordSpace={"mr-[0.5ch]"}
-              charSpace={"mr-[0.001em]"}
-              className="font-bold antialiased text-xl md:text-2xl mb-2"
-            >
+            <div className="font-bold antialiased text-xl md:text-2xl mb-2">
               Frontend Tools
-            </AnimatedTitle>
-            <AnimatedBody className="text-white/60 text-base md:text-xl leading-8">
+            </div>
+            <div className="text-white/60 text-base md:text-xl leading-8">
               JavaScript(ES6+), React, Next.js, TypeScript, Jotai, React Query,
               HTML5, Git/GitHub, React Hook Form.
-            </AnimatedBody>
+            </div>
           </div>
           <div>
-            <AnimatedTitle
-              wordSpace={"mr-[0.5ch]"}
-              charSpace={"mr-[0.001em]"}
-              className="font-bold antialiased text-xl md:text-2xl mb-2"
-            >
+            <div className="font-bold antialiased text-xl md:text-2xl mb-2">
               UI Libraries
-            </AnimatedTitle>
-            <AnimatedBody className="text-white/60 text-base md:text-xl leading-8">
+            </div>
+            <div className="text-white/60 text-base md:text-xl leading-8">
               CSS3/SCSS/SASS, Tailwind CSS, Styled Components, Bootstrap,
               Shadcn/ui
-            </AnimatedBody>
+            </div>
           </div>
           <div>
-            <AnimatedTitle
-              wordSpace={"mr-[0.5ch]"}
-              charSpace={"mr-[0.001em]"}
-              className="font-bold antialiased text-xl md:text-2xl mb-2"
-            >
+            <div className="font-bold antialiased text-xl md:text-2xl mb-2">
               Backend Tools
-            </AnimatedTitle>
-            <AnimatedBody className="text-white/60 text-base md:text-xl leading-8">
+            </div>
+            <div className="text-white/60 text-base md:text-xl leading-8">
               JavaScript(ES6+), TypeScript, Node.js, Nest.js, Express.js,
               Prisma, Typeorm, Python, Django, C#, .NET, SQL, REST API
-            </AnimatedBody>
+            </div>
           </div>
           <div>
-            <AnimatedTitle
-              wordSpace={"mr-[0.5ch]"}
-              charSpace={"mr-[0.001em]"}
-              className="font-bold antialiased text-xl md:text-2xl mb-2"
-            >
+            <div className="font-bold antialiased text-xl md:text-2xl mb-2">
               DevOps Tools
-            </AnimatedTitle>
-            <AnimatedBody className="text-white/60 text-base md:text-xl leading-8">
+            </div>
+            <div className="text-white/60 text-base md:text-xl leading-8">
               Docker, Azure, AWS, Vercel, Render
-            </AnimatedBody>
+            </div>
           </div>
-
           <div>
-            <AnimatedTitle
-              wordSpace={"mr-[0.5ch]"}
-              charSpace={"mr-[0.001em]"}
-              className="font-bold antialiased text-xl md:text-2xl mb-2"
-            >
+            <div className="font-bold antialiased text-xl md:text-2xl mb-2">
               Design Tools
-            </AnimatedTitle>
-            <AnimatedBody className="text-white/60 text-base md:text-xl leading-8">
+            </div>
+            <div className="text-white/60 text-base md:text-xl leading-8">
               Figma, FigJam, UI Design, Prototyping
-            </AnimatedBody>
+            </div>
           </div>
         </div>
       </div>
